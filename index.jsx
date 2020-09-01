ad0fea5ba945dd189a5d1ec832de9a997a4e24c9
Jake <jtschwantes@gmail.com>
2020-09-01 14:49:16 -0600

    removed hello world component

--- index.jsx
+++ index.jsx
@@ -2,8 +2,4 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import HomePage from 'pages/HomePage';
 
-const HelloComponent = () => {
-    return <h1>Hello World!</h1>
-}
-
 ReactDom.render(<HomePage />, document.getElementById('root'));
