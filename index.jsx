029dcd634c3fd911d12dce78997a03bf7ed51d1d
Jake <jtschwantes@gmail.com>
2020-09-01 14:46:32 -0600

    starting homepage

--- index.jsx
+++ index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import HomePage from 'pages/HomePage';
 
 const HelloComponent = () => {
     return <h1>Hello World!</h1>
 }
 
-ReactDom.render(<HelloComponent />, document.getElementById('root'));
+ReactDom.render(<HomePage />, document.getElementById('root'));
