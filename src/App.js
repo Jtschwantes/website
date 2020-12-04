4cbcf8ef696b1a918314a6391ec7500c9034ec30
Jake <jtschwantes@gmail.com>
2020-12-03 21:23:15 -0700

    trying something else....

--- src/App.js
+++ src/App.js
@@ -24,25 +24,25 @@ const separator = css`
 
 export default function App() {
     const [{data, loading, error}] = useAxios('https://still-journey-39405.herokuapp.com/projects');
-    // React.useEffect(() => {
-    //     // Google api load
-    //     const gapiScript = document.createElement('script')
-    //     gapiScript.src = 'https://apis.google.com/js/platform.js'
-    //     gapiScript.onload = () => {
-    //         window.gapi.load('auth2', () => {
-    //             window.gapi.auth2.init({
-    //                 client_id: '723588652665-thkoe5eonaumrjabu495nj4ca2fs2q7u.apps.googleusercontent.com'
-    //             })
-    //             window.gapi.load('signin2', () => {
-    //                 const params = {
-    //                     theme: 'dark',
-    //                     onsuccess: () => {console.log('Big Yay!')}
-    //                 }
-    //             })
-    //         })
-    //     }
-    //     document.body.appendChild(gapiScript)
-    // }, [])
+    React.useEffect(() => {
+        // Google api load
+        const gapiScript = document.createElement('script')
+        gapiScript.src = 'https://apis.google.com/js/platform.js'
+        gapiScript.onload = () => {
+            window.gapi.load('auth2', () => {
+                window.gapi.auth2.init({
+                    client_id: '723588652665-thkoe5eonaumrjabu495nj4ca2fs2q7u.apps.googleusercontent.com'
+                })
+                // window.gapi.load('signin2', () => {
+                //     const params = {
+                //         theme: 'dark',
+                //         onsuccess: () => {console.log('Big Yay!')}
+                //     }
+                // })
+            })
+        }
+        document.body.appendChild(gapiScript)
+    }, [])
     
     return (
       <Router>
