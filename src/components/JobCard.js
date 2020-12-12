afa9b79abb09328eb375ec502cefc819fc874911
Jake <jtschwantes@gmail.com>
2020-12-11 17:53:59 -0700

    added edit and delete buttons to job section

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -4,6 +4,8 @@
 import React from 'react'
 import {jsx, css} from '@emotion/core'
 import { formatDate } from '../services/utility'
+import Button from './Button'
+import { axiosDeleteJob } from '../services/axios'
 
 const card = css`
     background-color: #383855;
@@ -11,10 +13,22 @@ const card = css`
     margin: 5px;
     padding: 0px 10px;
     width: auto;
-    height: 150px;
+    height: 130px;
+`
+const editBtns = css`
+    position: absolute;
+    display: float;
+    bottom: 5px;
+    right: -5px;
 `
 
-export default function JobCard({ job }) {
+export default function JobCard({ job, isOwner, signedIn, data, setData }) {
+    const del = async() => {
+        await axiosDeleteJob(job.id, {token: signedIn, account_id: job.account_id})
+            .catch(console.error)
+        setData(JSON.parse(JSON.stringify({ ...data, jobs: data.jobs.filter(job => job.id != job.id)})))
+    }
+    
     return(
         <div css={card}>
             <p css={css`margin: 2px;`}>
@@ -28,6 +42,7 @@ export default function JobCard({ job }) {
                 <em>{job.employer}</em><br />
                 {job.description}
             </p>
+            {isOwner && <div css={editBtns}><Button text="Edit"/><Button text="Delete" onClick={del}/></div>}
         </div>
     )
 }
