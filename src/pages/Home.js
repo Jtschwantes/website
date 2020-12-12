894e266d520321aa08f99b89fb23b8e655471e4f
Jake <jtschwantes@gmail.com>
2020-12-12 12:56:47 -0700

    demonstration

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,6 +16,10 @@ export default function Home() {
   return(
     <>
       <h1>Welcome to My Website!</h1>
+      <h1>Welcome to My Website!</h1>
+      <h1>Welcome to My Website!</h1>
+      <h1>Welcome to My Website!</h1>
+      <h1>Welcome to My Website!</h1>
       <p>
           This website contains a description about my accomplishments as well as things that I'm passionate about. You can browse through a list of projects of things that I've done, both personally and professionally, or look at some of the other things that I've thrown on here. I hope that something catches your eye! If you wish to contact me, you can find my personal information in the <Link css={link} to="/about">about me</Link> section.
 
